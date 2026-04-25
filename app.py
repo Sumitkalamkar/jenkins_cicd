03255248d3140b8ba6b0c750eaeb381fd96dfa2b
Sumitkalamkar <167060805+Sumitkalamkar@users.noreply.github.com>
2026-04-25 10:09:23 +0530

    changed the code in app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def create_app():
     @app.route('/')
     def home():
         print("inside home function")
-        return 'Hurray Sudhanshu hurray1234!'
+        return 'hi i am sumit kalamkar welcome to my cicd pipeline.'
 
     @app.route('/test')
     def test():
